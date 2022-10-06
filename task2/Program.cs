37fa12f0633e0b5a08fec4bb3e02c7072b656c2d
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-07 00:21:14 +0700

    Семинар 1 Задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+Console.Write("Введите число a: ");
+int.TryParse(Console.ReadLine()!, out a);
+int b;
+Console.Write("Введите число b: ");
+int.TryParse(Console.ReadLine()!, out b);
+if (a>b)
+{
+    Console.Write($"Большее число = {a}, меньшее число = {b}");
+}
+else
+{
+    Console.Write($"Большее число = {b}, меньшее число = {a}");
+}
+
+
+
+
+
