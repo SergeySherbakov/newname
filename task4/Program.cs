5c5b322ce4398ddc7e47ae79c5bca2ec2e93fc07
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-07 00:27:38 +0700

    Семинар 1 Задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+Console.Write("Введите число a: ");
+int.TryParse(Console.ReadLine()!, out a);
+int b;
+Console.Write("Введите число b: ");
+int.TryParse(Console.ReadLine()!, out b);
+int c;
+Console.Write("Введите число c: ");
+int.TryParse(Console.ReadLine()!, out c);
+int max = 0;
+if (a > b && a > c)
+    max = a;
+else if (b > c && b > a)
+    max = b;
+else if (c > a && c > b)
+    max = c;
+Console.Write($"Большее число = {max}");
