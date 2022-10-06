f7f6d7ea1319f7797e71a8a2179b8ab198eba88a
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-07 00:38:53 +0700

    Семинар 1 Задача 6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+Console.Write("Введите число a: ");
+int.TryParse(Console.ReadLine()!, out a);
+int b = 2;
+if (a % b == 0)
+{
+    Console.Write($"Четное");
+}
+else
+{
+    Console.Write($"Нечетное");
+}
