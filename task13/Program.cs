0e45a942b9158813c2b4e52dddbddea555c6ecc6
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-09 17:22:29 +0700

    Семинар 2 задача 13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+int.TryParse(Console.ReadLine()!, out a);
+int temp = a;
+int k = 0;
+while(temp > 0)
+{
+    temp /= 10;
+    k++;
+}
+if(k > 2)
+   Console.Write($"{(a / (int)Math. Pow(10, k-3)) % 10}");
+else
+   Console.Write($"Третьей цифры нет");
