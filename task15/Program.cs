b7c38d519b31552ac01679bccde5ddcce6ac82c1
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-10 00:48:28 +0700

    Семинар 2 задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 0;
+Console.Write($"Введите число от 1 до 7: ");
+int.TryParse(Console.ReadLine()!, out a);
+if (a >= 1 && a < 6)
+    Console.Write($"Число является рабочим днем");
+else if (a > 5 && a < 8)
+    Console.Write($"Число является выходным днем");
+else
+    Console.Write($"Неверно введено число");
