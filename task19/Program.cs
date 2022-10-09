bac466d4c577c87a1f192ec54405236968dc58a8
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-10 00:46:06 +0700

    Семинар 3 задача 19

---
+++ task19/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 0;
+Console.Write($"Введите пятизначное число: ");
+int.TryParse(Console.ReadLine()!, out a);
+if(a / 10000 == a % 10 && a / 1000 % 10 == a / 10 % 10)
+   Console.Write($"Число является палиндромом");
+else
+   Console.Write($"Число не является палиндромом");
