3e598ab9e4231f133e2defe6db60e35faac078b1
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-11 22:27:07 +0700

    Семинар 4 задача 29

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array;
+array = CreateArray();
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+int[] CreateArray()
+{
+    int[] _array = new int[8];
+    for (int i = 0; i < 8; i++)
+        _array[i] = new Random().Next(100);
+    return _array;
+}
+
