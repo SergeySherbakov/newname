8438aeb016778db8f922c55cf46a784f480df4aa
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-11 22:25:14 +0700

    Семинар 4 задача 25

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿int A = 0;
+Console.Write($"Введите число A: ");
+int.TryParse(Console.ReadLine()!, out A);
+int B = 0;
+Console.Write($"Введите число B: ");
+int.TryParse(Console.ReadLine()!, out B);
+Console.Write($"Число {A} в степени {B} = {Mult(A)}");
+int Mult(int _A)
+{
+    int mult = 1;
+    for (int i = 1; i <= B; i++)
+        mult *= _A;
+    return mult;
+}
+
+
