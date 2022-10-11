4f9e767a8d8ac04bbf4ee2b719e55c937dd2ae72
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-11 22:26:33 +0700

    Семинар 4 задача 27

---
+++ task27/Program.cs
@@ -0,0 +1,9 @@
+﻿int rand = new Random().Next();
+Console.WriteLine(rand);
+int sum = 0;
+while (rand > 0)
+{
+    sum = sum + rand % 10;
+    rand = rand / 10;
+}
+Console.Write($"Сумма цифр = {sum}: ");
