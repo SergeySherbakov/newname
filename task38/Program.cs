0c354853a3b89e1d6a4bd1ce6afa39a434e0227b
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-14 21:52:13 +0700

    Семинар 5 задача 38

---
+++ task38/Program.cs
@@ -0,0 +1,20 @@
+﻿double[] array = new double[new Random().Next(6, 10)];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (double)new Random().Next(0, 10 * 10) / 10;
+    Console.Write($"{array[i]} ");
+}
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (min > array[i])
+        min = array[i];
+    if (max < array[i])
+        max = array[i];
+}
+
+Console.Write($"\nmin = {min} max = {max}  Разница между максимальным и минимальным  {max - min}");
