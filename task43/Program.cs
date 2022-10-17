d991bcdbf093b058bd67950280b0cb9b766e7ca8
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-17 11:42:25 +0700

    Семинар 6 задача 43

---
+++ task43/Program.cs
@@ -0,0 +1,20 @@
+﻿int b1;
+Console.Write($"Введите b1: ");
+int.TryParse(Console.ReadLine()!, out b1);
+
+int k1;
+Console.Write($"Введите k1: ");
+int.TryParse(Console.ReadLine()!, out k1);
+
+int b2;
+Console.Write($"Введите b2: ");
+int.TryParse(Console.ReadLine()!, out b2);
+
+int k2;
+Console.Write($"Введите k2: ");
+int.TryParse(Console.ReadLine()!, out k2);
+
+double x = (double)(b2 - b1) / (k1 - k2);
+double y = (double)k1 * x + b1;
+
+Console.Write($"Точка пересечения:({x}; {y})");
