64ea2b6d1a738ba9f6a8d3fd867d7f9c63d59e0d
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-17 11:41:48 +0700

    Семинар 6 задача 41

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿int n;
+Console.WriteLine($"Введите количество чисел: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+int[] array = new int[n];
+Console.WriteLine($"Введите числа: ");
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    int.TryParse(Console.ReadLine()!, out array[i]);
+    if (array[i] > 0)
+        sum++;
+}
+
+Console.Write($"Количество положительных чисел {sum}: ");
+
