b2a6ce25eb573a7d0735e5d2c93ee1842cdcc335
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-19 12:48:08 +0700

    Семинар 7 задача 50

---
+++ task50/Program.cs
@@ -0,0 +1,24 @@
+﻿int m = 4, n = 6;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(20);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int a, b;
+int.TryParse(Console.ReadLine()!, out a);
+b = a % 10;
+a /= 10;
+
+if (a >= array.GetLength(0) || b >= array.GetLength(1) || a < 0)
+    Console.Write($"Такого числа в массиве нет!");
+else
+{
+    Console.Write($"{array[a, b]}");
+}
