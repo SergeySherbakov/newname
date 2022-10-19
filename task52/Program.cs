46289144bdfa890960f6ffa1e1cdf40baf891d23
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-19 12:48:59 +0700

    Семинар 7 задача 52

---
+++ task52/Program.cs
@@ -0,0 +1,27 @@
+﻿int m = 4, n = 6;
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+for (int j = 0; j < n; j++)
+{
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"Среднее арифметическое {j + 1} столбца = {(double)sum / m}\n");
+}
+
+
+
