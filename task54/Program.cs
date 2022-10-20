8481f53453a2967ca2c8044273cc438cfd8c7da5
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-20 20:18:21 +0700

    Семинар 8 задача 54

---
+++ task54/Program.cs
@@ -0,0 +1,44 @@
+﻿int m = 4, n = 6;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        for (int z = 0; z < n - 1; z++)
+        {
+            if (array[i, z] < array[i, z + 1])
+            {
+                int temp = 0;
+                temp = array[i, z];
+                array[i, z] = array[i, z + 1];
+                array[i, z + 1] = temp;
+            }
+        }
+
+    }
+}
+Console.WriteLine();
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
