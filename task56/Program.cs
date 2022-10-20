005f04da3ad9c9f108252e7f7e64fa9a6351ec51
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-20 20:19:02 +0700

    Семинар 8 задача 56

---
+++ task56/Program.cs
@@ -0,0 +1,31 @@
+﻿int m = 4, n = 6;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int minsum = int.MaxValue;
+int k = 0;
+for (int i = 0; i < m; i++)
+{
+    int sum = 0;
+    for (int j = 0; j < n; j++)
+    {
+        sum = sum + array[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        k++;
+    }
+}
+
+Console.WriteLine($"строка с наименьшей суммой №:{k}, сумма чисел: {minsum}");
+
