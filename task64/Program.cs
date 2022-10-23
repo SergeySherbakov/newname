a2b2579291162e91f7b4627dbe64be044be2545b
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-23 16:35:18 +0700

    Семинар 9 задача 64

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+PrintNumbers(a, 1);
+
+void PrintNumbers(int _start, int _end)
+{
+    if (_start >= _end)
+    {
+        Console.Write($"{_start} ");
+        PrintNumbers(_start - 1, _end);
+    }
+}
