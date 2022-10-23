841de9257315a4e1206687a8140f6202f7e27300
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-23 16:36:39 +0700

    Семинар 9 задача 68

---
+++ task68/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int b;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write($"Функция Аккермана = {Ack(a, b)} ");
+
+int Ack(int _a, int _b)
+{
+    if (_a == 0)
+        return _b + 1;
+    else if (_b == 0)
+        return Ack(_a - 1, 1);
+    else return Ack(_a - 1, Ack(_a, _b - 1));
+}
+
