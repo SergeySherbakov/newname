85e47caefe7b29ea03b1c9dc1cf4372b929c9f73
SergeySherbakov <serega.alexandr@yandex.ru>
2022-10-23 16:36:06 +0700

    Семинар 9 задача 66

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int b;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write($"Сумма чисел между {a} и {b} = {SumNumbers(a, b)}");
+
+static int SumNumbers(int _start, int _end)
+{
+    if (_start == _end)
+        return _start;
+    return _end + SumNumbers(_start, _end - 1);
+}
